f0b838b836ee36eecb51f647a60fd90fc7a48dfb
lolifamily <lolifamilies@gmail.com>
2023-09-22 00:28:41 +0800

    Update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 const nextConfig = {
     reactStrictMode: true,
-    skipTrailingSlashRedirect: true,
+    trailingSlash: true,
     async headers() {
         return [{
             "source": "/(.*)",
